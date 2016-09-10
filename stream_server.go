79937e480d279211912a5e6f34ea28f6aaaf3565
kelvinabrokwa <kelvinabrokwa@gmail.com>
2016-09-10 14:29:30 -0400

    redo without channels

--- stream_server.go
+++ stream_server.go
@@ -1,141 +1,70 @@
 package streamserver
 
 import (
-    "log"
+    "net/http"
+    "github.com/gorilla/websocket"
+    "fmt"
     "strings"
     "strconv"
-    "net/http"
-    "encoding/json"
-
-    "github.com/gorilla/websocket"
+    "sync"
 )
 
-var sources = make(map[int]*DataSource)
-// TODO: protect this, not thread safe
-var sourceId = 0
-var endpoint = ""
-
-// allow CORS
 var upgrader = websocket.Upgrader{
     CheckOrigin: func(r *http.Request) bool {
         return true
     },
 }
 
-func Start(ep string) {
-    endpoint = ep
-    http.HandleFunc(endpoint + "/in", DataInHandler)
-    http.HandleFunc(endpoint + "/list", ListStreams)
-}
-
-type Client struct {
-    ws *websocket.Conn
-    data chan []byte
-    source *DataSource
-}
-
-func (c *Client) run() {
-    c.source.register <- c
-    defer func() {
-        c.source.unregister <- c
-    }()
-    for {
-        d := <- c.data
-        if err := c.ws.WriteMessage(websocket.BinaryMessage, d); err != nil {
-            log.Println(err)
-            log.Println("CLOSED: " + c.ws.RemoteAddr().String())
-            c.ws.Close()
-            return
-        }
-    }
-}
-
-type DataSource struct {
-    data chan []byte
-    clients map[*Client]bool
-    register chan *Client
-    unregister chan *Client
-    id int
-}
-
-func (v *DataSource) run() {
-    for {
-        select {
-        case client := <-v.register:
-            v.clients[client] = true
-        case client := <-v.unregister:
-            delete(v.clients, client)
-            close(client.data)
-        case data := <- v.data:
-            for client := range v.clients {
-                client.data <- data
-            }
-        }
-    }
-}
-
-func newDataSource() *DataSource {
-    sourceId++
-    return &DataSource{
-        data: make(chan []byte),
-        clients: make(map[*Client]bool),
-        register: make(chan *Client),
-        unregister: make(chan *Client),
-        id: sourceId,
-    }
-}
+var sourceId = 1;
+var sourceIdMutex = &sync.Mutex{}
+var sources = make(map[string]map[*websocket.Conn]bool)
 
 func DataInHandler(w http.ResponseWriter, r *http.Request) {
+    sourceIdMutex.Lock()
+    idStr := strconv.Itoa(sourceId);
+    sourceId++
+    sourceIdMutex.Unlock()
+    sources[idStr] = make(map[*websocket.Conn]bool)
+
     ws, err := upgrader.Upgrade(w, r, nil)
+
+    http.HandleFunc("/data/out/" + idStr, DataOutHandler)
+
     if err != nil {
-        log.Println(err)
+        fmt.Println(err)
         return
     }
-    ds := newDataSource()
-    // TODO: remove handler when the source goes away
-    http.HandleFunc(endpoint + "/out/" + strconv.Itoa(ds.id), DataOutHandler)
-    sources[ds.id] = ds
-    log.Println("new data source connected: " + strconv.Itoa(ds.id))
-    go ds.run()
-    go func() {
-        for {
-            _, p, err := ws.ReadMessage()
-            if err != nil {
-                log.Println(err)
-                delete(sources, ds.id)
-                return
-            }
-            ds.data <- p
+    for {
+        _, p, err := ws.ReadMessage()
+        if err != nil {
+            delete(sources, idStr)
+            fmt.Println(err)
+            return
         }
-    }()
+        for client := range sources[idStr] {
+            if err := client.WriteMessage(websocket.BinaryMessage, p); err != nil {
+                delete(sources[idStr], client)
+                fmt.Println(err)
+            }
+        }
+    }
 }
 
 func DataOutHandler(w http.ResponseWriter, r *http.Request) {
     url := strings.Split(r.URL.String(), "/")
-    reqSourceId, err := strconv.Atoi(url[len(url) - 1])
-    if err != nil {
-        log.Println(err)
-        return
-    }
-    log.Println("new client connected to: " + strconv.Itoa(reqSourceId))
+    sourceId := url[len(url) - 1]
     ws, err := upgrader.Upgrade(w, r, nil)
     if err != nil {
-        log.Println(err)
+        fmt.Println(err)
         return
     }
-    client := Client{
-        ws: ws,
-        data: make(chan []byte),
-        source: sources[reqSourceId],
+    if err := ws.WriteMessage(websocket.BinaryMessage, []byte{0x6a, 0x73, 0x6d, 0x70, 0x01, 0x40, 0x00, 0xf0}); err != nil {
+        fmt.Println(err)
+        return
     }
-    client.run()
+    sources[sourceId][ws] = true
 }
 
-func ListStreams(w http.ResponseWriter, r *http.Request) {
-    sourceIds := make([]int, 0, len(sources))
-    for k := range sources {
-        sourceIds = append(sourceIds, k)
-    }
-    encoder := json.NewEncoder(w)
-    encoder.Encode(sourceIds)
+func Start(endpoint string) {
+    http.HandleFunc(endpoint + "/in", DataInHandler)
 }
